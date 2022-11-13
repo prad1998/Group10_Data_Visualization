2d3ef894e01fcbdd91e44dd1ea370df5ce98a09b
Frederikh1 <42929762+Frederikh1@users.noreply.github.com>
2022-11-13 22:37:30 +0100

    use of ggplot for barplot

--- loadDataScript.R
+++ loadDataScript.R
@@ -1,5 +1,13 @@
 library(readr)
+library(ggplot2)
+
 cars <- read_csv("cars.csv")
 
-summary(cars)
+
+originCount <- table(cars$Origin, dnn = "Origin")
+#barplot(originCount, main = "Origin")
+test <- data.frame(originCount)
+colnames(test) <- c("Origin", "Frequency")
+p <- ggplot(data=test, aes(x=Frequency, y=Origin)) + geom_bar(stat="identity")
+p
 
