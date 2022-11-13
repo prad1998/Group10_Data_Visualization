85da4527371cb2a60eded27aa687c5866165ee47
Ahashm <ahmat18@student.sdu.dk>
2022-11-13 18:16:42 +0100

    Line chart plot added
    Plots the Toyota Corolla car + Pinto in one single ggplot line chart with different colors.
    
    The graph visualizes the difference between the different models and miles per gallon

--- loadDataScript.R
+++ loadDataScript.R
@@ -1,8 +1,6 @@
-#Libraries:
-library(readr) #Reader for csv.
-library(ggplot2) #GGplot for graphs
-
-#Reading Data
+library(readr)
+library(ggplot2)
+library(dplyr)
 cars <- read_csv("cars.csv")
 
 #Quick summary of data set
@@ -27,3 +25,25 @@ colnames(originDataFrame) <- c("Origin", "Frequency")
 p <- ggplot(data=originDataFrame, aes(x=Frequency, y=Origin)) + 
   geom_bar(stat="identity")
 p
+# line chart
+
+toyota <- filter(cars, Car == "Toyota Corolla")
+
+pinto <- filter(cars, Car == "Ford Pinto")
+
+toyota
+
+pinto
+
+
+ggplot(toyota, aes(x=toyota$Model, y=toyota$MPG, group=1)) +
+  geom_line()+
+  geom_point()
+
+
+
+ggplot(pinto, aes(x=pinto$Model, y=pinto$MPG, group=1)) +
+  geom_line()+
+  geom_point()
+
+
