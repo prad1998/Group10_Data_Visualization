c77321cad1d51f24fa94af6dd2882a296d469783
Frederikh1 <42929762+Frederikh1@users.noreply.github.com>
2022-11-13 22:41:00 +0100

    better readability and naming

--- loadDataScript.R
+++ loadDataScript.R
@@ -6,8 +6,9 @@ cars <- read_csv("cars.csv")
 
 originCount <- table(cars$Origin, dnn = "Origin")
 #barplot(originCount, main = "Origin")
-test <- data.frame(originCount)
-colnames(test) <- c("Origin", "Frequency")
-p <- ggplot(data=test, aes(x=Frequency, y=Origin)) + geom_bar(stat="identity")
+originDataFrame <- data.frame(originCount)
+colnames(originDataFrame) <- c("Origin", "Frequency")
+p <- ggplot(data=originDataFrame, aes(x=Frequency, y=Origin)) + 
+  geom_bar(stat="identity")
 p
 
