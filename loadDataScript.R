6b0b26bedc00cb0a50bea895aa1f820630626d88
KasperDelaxson <43139475+KasperDelaxson@users.noreply.github.com>
2022-11-14 16:09:24 +0100

    Update loadDataScript.R
    Script to load and show data set.
    Shows scatterplot using ggplot library.
    Data used Acceleration, Horsepower and Fuel consumption(MPG)

--- loadDataScript.R
+++ loadDataScript.R
@@ -1,5 +1,21 @@
-library(readr)
+#Libraries:
+library(readr) #Reader for csv.
+library(ggplot2) #GGplot for graphs
+
+#Reading Data
 cars <- read_csv("cars.csv")
 
+#Quick summary of data set
 summary(cars)
 
+#View Dataset raw data
+View(cars)
+
+#Variables
+Acceleration<-cars$Acceleration
+Fuel <- cars$MPG
+Horsepower<-cars$Horsepower
+
+#Plots using ggplot
+ggplot(data=cars, aes(x=Acceleration, y=Fuel)) + geom_point(size=1)
+ggplot(data=cars, aes(x=Acceleration, y=Horsepower)) + geom_point(size=1)
