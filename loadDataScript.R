c76eba54cfd7cf03979e029aa4be8a1f38ef2906
Frederikh1 <42929762+Frederikh1@users.noreply.github.com>
2022-11-20 22:05:54 +0100

    histogram

--- loadDataScript.R
+++ loadDataScript.R
@@ -5,10 +5,19 @@ cars <- read_csv("cars.csv")
 
 
 originCount <- table(cars$Origin, dnn = "Origin")
-#barplot(originCount, main = "Origin")
 originDataFrame <- data.frame(originCount)
 colnames(originDataFrame) <- c("Origin", "Frequency")
 p <- ggplot(data=originDataFrame, aes(x=Frequency, y=Origin)) + 
   geom_bar(stat="identity")
 p
+
+mpgTable <- as.numeric(cars$MPG)
+mpgTable
+mpgTable <- cut(mpgTable, breaks=c(0, 5, 10, 15, 20, 25, 30, 35, 40))
+
+mpgDf <- data.frame(mpgTable)
+
+histo <- ggplot(mpgDf, aes(x=mpgTable)) + 
+  geom_bar()
+histo
 
