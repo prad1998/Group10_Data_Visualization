ce2107597cf75a7fb2ac97f68ed8d3391f22b34e
Pradeep Thayaparan <prtha18@student.sdu.dk>
2022-11-20 14:31:24 +0100

    Static and animated grouped boxplot added

--- loadDataScript.R
+++ loadDataScript.R
@@ -27,3 +27,46 @@ colnames(originDataFrame) <- c("Origin", "Frequency")
 p <- ggplot(data=originDataFrame, aes(x=Frequency, y=Origin)) + 
   geom_bar(stat="identity")
 p
+
+
+######## Boxplot 
+
+library(readr) 
+library(ggplot2) 
+library(dplyr)
+library(gganimate)
+
+
+#Reading Data
+cars <- read_csv("cars.csv")
+
+amc <- filter(cars, Car == "AMC Matador")
+
+#static grouped boxplots for region  
+
+ggplot(cars, aes(x=as.factor(Origin), y=MPG)) + 
+  geom_boxplot(fill="slateblue", alpha=0.2) + 
+  xlab("Origin")
+
+
+#animated grouped boxplots for region that have Year as state   
+ggplot(cars, aes(factor(Origin), MPG)) + 
+  geom_boxplot(fill="slateblue", alpha=0.2) + 
+  # The gganimate code
+  transition_states(
+    Model,  
+    transition_length = 20,
+    state_length = 1
+  ) + labs(title = "Year: 19{closest_state}")  +
+  
+  # Used to control the non-persistent data during a tween  
+  enter_fade() + 
+  exit_shrink()
+  
+
+
+
+
+
+
+
