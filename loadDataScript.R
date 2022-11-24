6bf903a5d5dda6ae97e3be22e4c3ccea6cf7cc4a
KasperDelaxson <43139475+KasperDelaxson@users.noreply.github.com>
2022-11-24 21:12:23 +0100

    Update loadDataScript.R
    Added more scatterplots on data.
    Added comments for better understanding.

--- loadDataScript.R
+++ loadDataScript.R
@@ -1,6 +1,7 @@
 #Libraries:
 library(readr) #Reader for csv.
 library(ggplot2) #GGplot for graphs
+library(ggpubr) #Regression and R^2
 
 #Reading Data
 cars <- read_csv("cars.csv")
@@ -13,9 +14,39 @@ View(cars)
 
 #Variables
 Acceleration<-cars$Acceleration
-Fuel <- cars$MPG
+MilesPerGallon <- cars$MPG
 Horsepower<-cars$Horsepower
+Weight<-cars$Weight
+Displacement<-cars$Displacement
+Cylinders<-cars$Cylinders
 
 #Plots using ggplot
-ggplot(data=cars, aes(x=Acceleration, y=Fuel)) + geom_point(size=1)
-ggplot(data=cars, aes(x=Acceleration, y=Horsepower)) + geom_point(size=1)
+#Correlation between Acceleration and MPG
+ggplot(data=cars, aes(x=Acceleration, y=MilesPerGallon)) + geom_point() +
+geom_smooth(method = lm) +
+  stat_regline_equation(label.y = 60, aes(label = ..eq.label..)) +  
+  stat_regline_equation(label.y = 50, aes(label = ..rr.label..))
+
+#Correlation between Acceleration and Horsepower
+ggplot(data=cars, aes(x=Acceleration, y=Horsepower)) + geom_point() + 
+  geom_smooth(method = lm) +
+  stat_regline_equation(label.y = 70, aes(label = ..eq.label..)) + 
+  stat_regline_equation(label.y = 50, aes(label = ..rr.label..))
+
+#Correlation between Acceleration and Cylinders
+ggplot(data=cars, aes(x=Acceleration, y=Cylinders)) + geom_point() + 
+  geom_smooth(method = lm) +
+  stat_regline_equation(label.y = 20, aes(label = ..eq.label..)) + 
+  stat_regline_equation(label.y = 15, aes(label = ..rr.label..))
+
+#Correlation between Acceleration and Weight
+ggplot(data=cars, aes(x=Acceleration, y=Weight)) + geom_point() + 
+  geom_smooth(method = lm) +
+  stat_regline_equation(label.y = 900, aes(label = ..eq.label..)) + 
+  stat_regline_equation(label.y = 800, aes(label = ..rr.label..))
+
+#Correlation between Acceleration and Displacement
+ggplot(data=cars, aes(x=Acceleration, y=Displacement)) + geom_point() + 
+  geom_smooth(method = lm) +
+  stat_regline_equation(label.y = 750, aes(label = ..eq.label..)) + 
+  stat_regline_equation(label.y = 700, aes(label = ..rr.label..))
