e7c90b15f738d159ca130ad1b4ae1bec4cd4275c
KasperDelaxson <43139475+KasperDelaxson@users.noreply.github.com>
2022-11-27 20:18:35 +0100

    Update loadDataScript.R
    Updated

--- loadDataScript.R
+++ loadDataScript.R
@@ -36,31 +36,32 @@ ggPlotFunction(Cylinders, MilesPerGallon)
 ggPlotFunction(Weight, MilesPerGallon)
 ggPlotFunction(Displacement, MilesPerGallon)
 
-ggplot(data=cars, aes(x=Acceleration, y=MilesPerGallon)) + geom_point() +
+#Correlation between Acceleration and MPG
+ggplot(data=cars, aes(x=MilesPerGallon, y=Acceleration)) + geom_point() +
 geom_smooth(method = lm) +
-  stat_regline_equation(label.y = 60, aes(label = ..eq.label..)) +  
-  stat_regline_equation(label.y = 50, aes(label = ..rr.label..))
+  stat_regline_equation(label.y = 35, aes(label = ..eq.label..)) +  
+  stat_regline_equation(label.y = 30, aes(label = ..rr.label..))
 
-#Correlation between Acceleration and Horsepower
-ggplot(data=cars, aes(x=Acceleration, y=Horsepower)) + geom_point() + 
+#Correlation between MPG and Horsepower
+ggplot(data=cars, aes(x=MilesPerGallon, y=Horsepower)) + geom_point() + 
   geom_smooth(method = lm) +
   stat_regline_equation(label.y = 70, aes(label = ..eq.label..)) + 
   stat_regline_equation(label.y = 50, aes(label = ..rr.label..))
 
-#Correlation between Acceleration and Cylinders
-ggplot(data=cars, aes(x=Acceleration, y=Cylinders)) + geom_point() + 
+#Correlation between MPG and Cylinders
+ggplot(data=cars, aes(x=MilesPerGallon, y=Cylinders)) + geom_point() + 
   geom_smooth(method = lm) +
-  stat_regline_equation(label.y = 20, aes(label = ..eq.label..)) + 
+  stat_regline_equation(label.y = 17, aes(label = ..eq.label..)) + 
   stat_regline_equation(label.y = 15, aes(label = ..rr.label..))
 
-#Correlation between Acceleration and Weight
-ggplot(data=cars, aes(x=Acceleration, y=Weight)) + geom_point() + 
+#Correlation between MPG and Weight
+ggplot(data=cars, aes(x=MilesPerGallon, y=Weight)) + geom_point() + 
   geom_smooth(method = lm) +
-  stat_regline_equation(label.y = 900, aes(label = ..eq.label..)) + 
+  stat_regline_equation(label.y = 1000, aes(label = ..eq.label..)) + 
   stat_regline_equation(label.y = 800, aes(label = ..rr.label..))
 
-#Correlation between Acceleration and Displacement
-ggplot(data=cars, aes(x=Acceleration, y=Displacement)) + geom_point() + 
+#Correlation between MPG and Displacement
+ggplot(data=cars, aes(x=MilesPerGallon, y=Displacement)) + geom_point() + 
   geom_smooth(method = lm) +
   stat_regline_equation(label.y = 750, aes(label = ..eq.label..)) + 
   stat_regline_equation(label.y = 700, aes(label = ..rr.label..))
